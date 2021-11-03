0391f21e6d8410bfdad6b0d4e9d99e86c1dd85a8
Ivan <ivancolombo98.ic@gmail.com>
2021-11-03 17:40:26 -0300

    Validacao status do usuario ao logar

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,43 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {            
+            if (Auth::user()->status == false) {                
+                $this->logout($request);
+                Session::flash('error', 'Usuário está inativo!');
+                return redirect()->back()->withInput();
+            }
+                        
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        Session::flash('error', 'Usuário ou senha incorretos!');
+        return redirect()->back()->withInput();
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,26 +5,18 @@
         <div class="vh-100 row justify-content-center align-items-center">
             <div class="col-md-5">
                 <div class="card p-4">
-                    <div class="card-header text-center mb-4 bg-transparent">{{ __('LOGIN') }}</div>
+                    <div class="card-header text-center mb-4 bg-transparent">
+                        {{ __('LOGIN') }}
+                    </div>
 
                     <div class="card-body">
-                        @if ($errors->any())
-                            @foreach ($errors->all() as $error)
-                                <div class="form-group row">
-                                    <div class="col-md-12 text-center">
-                                        <small class="text-danger">{{ $error }}</small>
-                                    </div>
-                                </div>
-                            @endforeach
-                        @endif
                         <form method="POST" action="{{ url('login') }}">
                             @csrf
-
                             <div class="form-group row">
                                 <div class="col-md-12">
                                     <input id="email" type="text" class="form-control" name="email"
-                                        value="{{ old('email') }}" required placeholder="E-mail"
-                                        autocomplete="email" autofocus>
+                                        value="{{ old('email') }}" required placeholder="E-mail" autocomplete="email"
+                                        autofocus>
 
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -47,7 +39,13 @@
                                     @enderror
                                 </div>
                             </div>
-                            
+                            @if (Session::has('error'))
+                                <div class="form-group row">
+                                    <div class="col-md-12 text-center">
+                                        <small class="text-danger">{{ Session::get('error') }}</small>
+                                    </div>
+                                </div>
+                            @endif
                             <div class="form-group row mb-0">
                                 <div class="col-md-12">
                                     <button type="submit" class="btn btn-primary w-100">
@@ -65,9 +63,10 @@
 @section('css')
     <style>
         #app {
-            background-image: url("/storage/login/imagem-login.webp");                 
+            background-image: url("/storage/login/imagem-login.webp");
             background-position: center;
-            background-size: cover;            
+            background-size: cover;
         }
+
     </style>
 @endsection
